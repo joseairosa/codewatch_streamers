f7fc94bbac37a6c7ffcbf50d7322c32029dc3a2e
José P. Airosa <me@joseairosa.com>
2015-07-19 23:17:10 +0100

    No need for the extra folder

--- config/puppet/templates/record_record_done.sh
+++ config/puppet/templates/record_record_done.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/bin/yamdi -i $1 -o /var/recordings/$2
-/usr/local/bin/aws s3 cp /var/recordings/$2 s3://codewatch-tv/recordings/$4/$2
+/usr/local/bin/aws s3 cp /var/recordings/$2 s3://codewatch-tv/recordings/$2
 /usr/bin/ffmpeg -y -i /var/recordings/$2 -ss 00:00:01 -vframes 1 /var/images/recording_thumbnails/$3.png
 /usr/bin/convert /var/images/recording_thumbnails/$3.png -resize 1280x720 /var/images/recording_thumbnails/$3.png
 /usr/local/bin/aws s3 cp /var/images/recording_thumbnails/$3.png s3://codewatch-tv/recording-thumbnails/$3.png
